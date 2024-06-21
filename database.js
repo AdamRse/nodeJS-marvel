05d4ad27487fb34fd992dc6dce19d539cf8cc825
AdamRse <bakaarion3@gmail.com>
2024-06-21 10:58:46 +0200

    commentaires

--- database.js
+++ database.js
@@ -1,17 +1,13 @@
 // Importer le module MySQL
 const mysql = require("mysql");
+
 var connection = mysql.createConnection({
     host     : '5.39.77.77',
     user     : 'tp-marvel',
     password : '0LZ_vIA4GJUSCnu/',
     database : 'tp-marvel'
-  });
-   
-  connection.connect();
-   
-  connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-  });
+});
+
+connection.connect();
 
 module.exports = connection;
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const mustacheExpress = require("mustache-express");
+const express = require("express"); // Simplifie  les appels serveur
+const mustacheExpress = require("mustache-express");// Externaliser le html (twig)
+const multer = require("multer");// Gère l'upload et le stockage de fichiers uploadés
+const path = require("path");// Fournit un accès aux dossiers et fichiers du serveur
+
+//Appels modules et fichiers
 const db = require("./database");
-const multer = require("multer");
-const path = require("path");
 const app = express();
 
-
-// Configurer le stockage des fichiers
+// Configurer le stockage des fichiers vial le module multer
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'images/personnages'); // Dossier où les fichiers seront stockés
@@ -15,46 +16,47 @@ const storage = multer.diskStorage({
         cb(null, Date.now() + path.extname(file.originalname)); // Nom du fichier
     }
 });
-
 const upload = multer({ storage: storage });
 
-app.engine("mustache", mustacheExpress());
-app.set("view engine", "mustache");
-app.set("views", __dirname + "/views");
+//app config
+//engine() : utilise un moteur supplémentaire avec express
+app.engine("mustache", mustacheExpress());// Ajouter Mustache à express
+//set() : paramètre les valeurs des settings de express (comme ini_set() de php)
+app.set("view engine", "mustache");// change la valeur de l'option view engine pour mustache (ajouter mustache à express dans les settings)
+app.set("views", __dirname + "/views");// Set le dossier views de mustache
+//use() : monte un middleware
+app.use(express.static("public"));// autoriser l'appel de fichiers dans le répertoire "/public" (./css/global.css par exemple) (par défaut l'accès est interdit à tous les fichiers)
+app.use(express.urlencoded({ extended: true }));// permet d'utiliser des données POST plus complètes (ex : person[age]=34&person[weight]=100)
 
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use('/images', express.static(__dirname + '/images'));
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
 
-// Route pour afficher le formulaire d'ajout de personnage
+// Routes
+//req : informations sur la requpête (req.query, req.params, req.body, req.headers, req.method, req.url, req.cookies)
+//res : methodes pour envoyer des réponses au client (une seule possible) (res.send(), res.json(), res.render(), res.redirect(), res.statut(), res.set(), res.cookie())
 app.get('/ajouterPersonnage', (req, res) => {
     res.render('ajouterPersonnage');
 });
-
-// Route pour gérer l'ajout de personnage
 app.post('/ajouterPersonnage', upload.single('avatar'), (req, res) => {
     const nom = req.body.nom;
     const description = req.body.description;
     const avatar = req.file.filename;
 
-    // Insertion dans la base de données
     db.query('INSERT INTO personnages (nom, description, photo) VALUES (?, ?, ?)', [nom, description, avatar], (error, results) => {
         if (error) throw error;
         res.redirect('/personnages');
     });
 });
-
 app.get('/', (req, res) => {
     res.send('Hello <p><a href="/personnages">Liste des personnages</a></p><p><a href="/ajouterPersonnage">Ajouter un personnages</a>');
 });
-
 app.get('/personnages', (req, res) => {
     db.query('SELECT * FROM personnages', (error, results) => {
         if (error) throw error;
         res.render('personnages', { personnages: results });
     });
 });
-
 app.get('/personnages/:id', (req, res) => {
     const id = req.params.id;
     db.query('SELECT * FROM personnages WHERE id = ?', [id], (error, results) => {
@@ -65,8 +67,4 @@ app.get('/personnages/:id', (req, res) => {
             res.status(404).send('Personnage non trouvé');
         }
     });
-});
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
 });
